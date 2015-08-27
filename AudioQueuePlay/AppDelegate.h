b4dc08fbad6aae4bd4e915bb4e1e4ccf4021faa3
zhanglei <zhanglei1216@163.com>
2015-08-27 11:26:32 +0800

    Initial Commit

---
+++ AudioQueuePlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AudioQueuePlay
+//
+//  Created by foreveross－bj on 15/8/27.
+//  Copyright (c) 2015年 foreveross－bj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
